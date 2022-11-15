4e8df18056d15774b3ea30cea56b946c3dff75e4
itzVipin <vkprajapati665@gmail.com>
2022-11-16 01:30:53 +0530

    Variables

---
+++ 03_variables.py
@@ -0,0 +1,21 @@
+# A variable is created the moment you first assign a value to it.
+
+# Creating Variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+# Casting
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+# Variable Names
+# Camel Case 
+myVariableName = "John"
+# Pascal Case 
+MyVariableName = "John"
+# Snake Case
+my_variable_name = "John"
