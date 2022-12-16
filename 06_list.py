2a9b7531aa2b76ec5117628f621b55c8a2338c68
Vipin <vkprajapati665@gmail.com>
2022-12-16 23:20:53 +0530

    Python

---
+++ 06_list.py
@@ -0,0 +1,21 @@
+# Lists are used to store multiple items in a single variable.
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# List items are ordered, changeable, and allow duplicate values.
+# List items are indexed, the first item has index [0], the second item has index [1] etc.
+# lists are indexed, lists can have items with the same value.
+
+# To determine how many items a list has, use the len() function:
+print(len(thislist))
+
+# List items can be of any data type:
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+# A list can contain different data types:
+list1 = ["abc", 34, True, 40, "male"]
+
+
+
