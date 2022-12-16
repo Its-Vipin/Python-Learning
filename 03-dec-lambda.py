9b7a290a64c6997445fbe551724084ad2a20793c
Vipin <vkprajapati665@gmail.com>
2022-12-16 23:20:33 +0530

    Pyhton

---
+++ 03-dec-lambda.py
@@ -0,0 +1,15 @@
+# list1=[3,4,5,8,9,45,6]
+
+# big=list(map(lambda i : i+5, list1))
+# print(big)
+
+
+# list1=[10,20,30,40,50]
+
+# print(list(map(lambda i : i*3,list1)))
+
+list2=['a','B','C','D','e','f']
+# list1=['a','e','f']
+# list3=['B','C','D']
+# print(list(map((lambda i:i.upper() , list1),(lambda j:j.lower(), list3))))
+
